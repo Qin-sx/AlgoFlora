9f571b643803f29ebcda5acc3d3362db8dd3558f
Qin-sx <Qin_sx@outlook.com>
2025-03-12 23:37:20 +0800

    added array string
    	modified:   array/string.cpp

--- array/string.cpp
+++ array/string.cpp
@@ -27,40 +27,7 @@ public:
     }
 };
 
-class Solution_49 {
-    string get_signature(const string& s) {
-        vector<int> count(26, 0);
-        for (char c : s) {
-            count[c - 'a']++;
-        }
 
-        stringstream ss;
-        for (int i = 0; i < 26; i++) {
-            if (count[i] != 0) {
-                ss << (char)('a' + i) << count[i];
-            }
-        }
-        return ss.str();
-    }
-
-public:
-    vector<vector<string>> groupAnagrams(vector<string>& strs) {
-        vector<vector<string>> ans;
-        unordered_map<string, vector<string>> groups;
-
-        for (int i = 0; i < strs.size(); ++i)
-        {
-            auto sign = get_signature(strs[i]);
-            groups[sign].emplace_back(strs[i]);
-        }
-
-        for (auto g : groups)
-        {
-            ans.emplace_back(g.second);
-        }
-        return ans;
-    }
-};
 
 int main()
 {
@@ -69,20 +36,6 @@ int main()
     vector<int> nums = {-1, 0, 3, 5, 9, 12};
     int target = 9;
     cout << s704.search(nums, target) << endl;
-    
-
-    Solution_49 s49;
-    vector<string> strs = {"eat", "tea", "tan", "ate", "nat", "bat"};
-    auto ans = s49.groupAnagrams(strs);
-    for (auto v : ans)
-    {
-        for (auto s : v)
-        {
-            cout << s << " ";
-        }
-        cout << endl;
-    }
-
 
     return 0;
 }
